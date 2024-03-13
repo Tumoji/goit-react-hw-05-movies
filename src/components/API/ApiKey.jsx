ade1af64a357333927a34dba1e89285c26c5d8a9
Tumoji <133051676+Tumoji@users.noreply.github.com>
2024-03-13 23:06:29 +0200

    final

---
+++ src/components/API/ApiKey.jsx
@@ -0,0 +1,2 @@
+export const API_KEY = '72fb4ce318ea231f82c7f71796aa3c6e';
+export const API_BASE_URL = 'https://api.themoviedb.org/3';
--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,35 @@
-export const App = () => {
+import React, { Suspense, lazy } from 'react';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from 'react-router-dom';
+import Navbar from './Navbar/Navbar';
+
+const Home = lazy(() => import('./Pages/Home'));
+const Movies = lazy(() => import('./Pages/Movies'));
+const MovieDetails = lazy(() => import('./Pages/MovieDetails'));
+const Cast = lazy(() => import('./Pages/Cast'));
+const Reviews = lazy(() => import('./Pages/Reviews'));
+
+const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <Router>
+      <Navbar />
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/movies" element={<Movies />} />
+          <Route path="/movies/:movieId" element={<MovieDetails />}>
+            <Route path="cast" element={<Cast />} />
+            <Route path="reviews" element={<Reviews />} />
+          </Route>
+          <Route path="*" element={<Navigate replace to="/" />} />
+        </Routes>
+      </Suspense>
+    </Router>
   );
 };
+
+export default App;
